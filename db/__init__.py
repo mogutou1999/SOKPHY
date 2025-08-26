998f5b5822d896058874044ba42c7eebfff04ff5
立减金 <langnadu1991@icloud.com>
2025-08-27 02:59:45 +0700

    Update __init__.py

--- db/__init__.py
+++ db/__init__.py
@@ -3,14 +3,15 @@
 from .base import Base
 from .models import User, Product, CartItem, Order, OrderItem
 from .crud import UserCRUD, ProductCRUD, CartCRUD, OrderCRUD
-from .session import async_session_maker
+from .session import async_session_maker, get_async_session  
 
 class MessageResponse:
     def __init__(self):
-        self.db = session()
+        self.db = async_session_maker()
+
 
     def get_data(self):
-        return self.db.query()
+        return self.db
 
 
 
