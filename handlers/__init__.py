4d478949b3d4fe160ec0ec0c08f24cd3c79704bc
立减金 <langnadu1991@icloud.com>
2025-09-05 23:11:10 +0700

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,16 +1,4 @@
 # handlers/__init__.py
-
-from aiogram import Router
-
-from .auth import setup_auth_handlers
-
-
-from .profile import setup_profile_handlers
-from .carts import setup_cart_handlers
-from .carts import setup_cart_handlers
-from .orders import setup_orders_handlers
-from .admin import setup_admin_handlers
-from .commands import setup_command_handlers# handlers/__init__.py
 from aiogram import Router
 
 from .admin_products import router as admin_products_router
@@ -40,37 +28,3 @@ def setup_all_handlers(dp: Router):
     dp.include_router(commands_router)
     dp.include_router(errors_router)
     dp.include_router(admin_products_router)
-from .errors import setup_error_handlers
-from .auth import setup_auth_handlers
-from .profile import setup_profile_handlers
-from aiogram import Router,Dispatcher
-from .products import setup_products_handlers
-from .buttons import router as buttons_router
-
-def setup_all_handlers(dp: Router):
-    dp.include_router(buttons_router)
-    setup_auth_handlers(dp)
-    setup_admin_handlers(dp)
-    setup_orders_handlers(dp)
-    setup_profile_handlers(dp)
-    setup_command_handlers(dp)
-    setup_error_handlers(dp)
-    setup_products_handlers(dp)
-    setup_cart_handlers(dp)
-   
-
-
-def setup_all_handlers() -> Router:
-    """
-    创建并组合所有路由
-    """
-    router = Router()
-    setup_auth_handlers(router)
-    
-  
-    setup_profile_handlers(router)
-    setup_cart_handlers(router)
-    return router
-    return router
-
-    
