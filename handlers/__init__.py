0347d94eded2982cc2e7e706985771fb85b2034e
立减金 <langnadu1991@icloud.com>
2025-09-06 00:24:34 +0700

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,6 +1,6 @@
 # handlers/__init__.py
 from aiogram import Router
-
+from .start import router as start_router
 from .admin_products import router as admin_products_router
 from .auth import router as auth_router
 from .menu import router as menu_router
@@ -12,11 +12,16 @@ from .profile import router as profile_router
 from .buttons import router as buttons_router
 from .commands import router as commands_router
 from .errors import router as errors_router
+from .payment import router as payment_router
+
+
 
 def setup_all_handlers(dp: Router):
     """
     统一注册所有 aiogram handler routers
     """
+    dp.include_router(payment_router)
+    dp.include_router(start_router)
     dp.include_router(buttons_router)
     dp.include_router(auth_router)
     dp.include_router(menu_router)
